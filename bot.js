6537cfbbc84885fed1228afdc14ea34dc2ef9082
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2019-01-04 13:45:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -11,7 +10,7 @@ logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.token,
    autorun: true
 });
 bot.on('ready', function (evt) {
